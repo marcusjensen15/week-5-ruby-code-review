f4e1a8e3905315c6ff20b2c6a47224e4fcc8a1d1
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-24 09:34:25 -0800

    add user authentication to project. Will begin adding admin features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_171233) do
+ActiveRecord::Schema.define(version: 2020_01_24_165915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2020_01_17_171233) do
     t.string "content_body"
     t.integer "rating"
     t.integer "product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
   end
 
   add_foreign_key "reviews", "products"
